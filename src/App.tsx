c1c1410babf4600bc65561f22c728d5c7cc69191
web223dev <ruslanus866@outlook.com>
2024-07-30 22:26:22 +0800

    Crated Custom Input component- Wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Input from './components/input';
+
 function App() {
-  return <div>Let's Start</div>;
+  return (
+    <main>
+      <Input id='name' label='Your name' type='text' />
+      <Input id='age' label='Your age' type='number' />
+    </main>
+  );
 }
 
 export default App;
