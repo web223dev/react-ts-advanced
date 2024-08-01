ff3325ff07a964a6507d1835f21f690b5e44e1b8
web223dev <ruslanus866@outlook.com>
2024-08-01 23:40:39 +0800

    Custom Button, Container(Polymorphic) wrapper components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import Input from './components/input';
+import Button from './components/Button';
+import Container from './components/Container';
+// import Input from './components/input';
 
 function App() {
   return (
     <main>
-      <Input id='name' label='Your name' type='text' />
-      <Input id='age' label='Your age' type='number' />
+      {/* <Input id='name' label='Your name' type='text' />
+      <Input id='age' label='Your age' type='number' /> */}
+      {/* <p>
+        <Button target=''>A Button</Button>
+      </p>
+      <p>
+        <Button href='https://google.com'>A Link</Button>
+      </p> */}
+      <Container as={Button} type='button'>
+        Click Me
+      </Container>
     </main>
   );
 }
