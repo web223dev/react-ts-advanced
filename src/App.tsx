f527b8f4658d936ab30f9829d0745778c0023f1a
web223dev <ruslanus866@outlook.com>
2024-08-03 16:42:32 +0800

    add ref using forwardRef in Input wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import Button from './components/Button';
-import Container from './components/Container';
+import { useRef } from 'react';
+// import Button from './components/Button';
+// import Container from './components/Container';
+import Input from './components/input';
 // import Input from './components/input';
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      {/* <Input id='name' label='Your name' type='text' />
-      <Input id='age' label='Your age' type='number' /> */}
-      {/* <p>
+      <Input id='name' label='Your name' type='text' ref={input} />
+      <Input id='age' label='Your age' type='number' />
+      {/* <p> 
         <Button target=''>A Button</Button>
       </p>
       <p>
         <Button href='https://google.com'>A Link</Button>
       </p> */}
-      <Container as={Button} type='button'>
+      {/* <Container as={Button} type='button'>
         Click Me
-      </Container>
+      </Container> */}
     </main>
   );
 }
