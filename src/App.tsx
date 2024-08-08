7f18aa4b615bff5d2e6ed1bb21cc96913ff4546f
web223dev <ruslanus866@outlook.com>
2024-08-08 15:13:06 +0800

    Crated Custom Form component- Wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { useRef } from 'react';
-// import Button from './components/Button';
+// import { useRef } from 'react';
 // import Container from './components/Container';
+import Button from './components/Button';
 import Input from './components/input';
-// import Input from './components/input';
+import Form, { FormHandle } from './components/Form';
+import { useRef } from 'react';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+  // const input = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+
   return (
     <main>
-      <Input id='name' label='Your name' type='text' ref={input} />
-      <Input id='age' label='Your age' type='number' />
-      {/* <p> 
-        <Button target=''>A Button</Button>
-      </p>
-      <p>
-        <Button href='https://google.com'>A Link</Button>
-      </p> */}
-      {/* <Container as={Button} type='button'>
-        Click Me
-      </Container> */}
+      <Form onSave={handleSave} ref={customForm}>
+        <Input id='name' label='Your name' type='text' />
+        <Input id='age' label='Your age' type='number' />
+        {/* <Input id='name' label='Your name' type='text' ref={input} /> */}
+        <p>
+          <Button>Save</Button>
+        </p>
+        {/* <p>
+          <Button href='https://google.com'>A Link</Button>
+        </p>
+        <Container as={Button} type='button'>
+          Click Me
+        </Container> */}
+      </Form>
     </main>
   );
 }
