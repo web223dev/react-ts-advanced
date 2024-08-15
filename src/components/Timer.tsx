20e72017037c8136e1fd6a958e3a7af8e35d2a3e
web223dev <ruslanus866@outlook.com>
2024-08-15 20:22:54 +0800

    Add timer progress function

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { type Timer as TimerProps } from '../store/timers-context';
 import Container from './UI/Container';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
